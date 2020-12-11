7b0abf0de5345168114470660ac3006d034d3ac3
raybednara <ray.bednara@att.net>
2020-12-10 18:01:22 -0600

    import classes and constructors, then create instances of each

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-import { greeting } from "./test";
+const Animal = require("./animal");
+const Dog = require("./dog");
+const Turtle = require("./turtle");
 
-console.log(greeting);
+const steve = new Animal({
+  species: "Bear",
+  diet:["fish", "deer", "plants", "berries"],
+  size: 1,
+  sound: "roar",
+  speed: 1,
+});
 
-const me = {
-  name: "Mark",
-};
+console.log(steve.info);;
+console.log(steve.speak());
+steve.addNewFoodToDiet("Porridge");
+console.log(steve.info);
 
-const { name } = me;
+
+const ares = new Dog({
+  species:"dog",
+  diet = ["dog food", "steak"],
+  size:0,
+  sound:"woof",
+  speed:1,
+  name:"Ares",
+  frisbee:1
+});
+
+console.log(ares.play());
+console.log(ares.speak());
+
+const michael = new Turtle({
+  species: "turtle",
+  diet = ["lettuce"],
+  size: 0,
+  sound= "silence",
+  speed: 0,
+  name: "Michael",
+  help:1
+})
+
+console.log(michael.info);
+console.log(michael.needsHelp());
